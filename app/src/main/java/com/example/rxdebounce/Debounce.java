1e1738838160f14edc3f4e8a8ab0fe61ff182533
Rajesh <rajeshswayampu96@gmail.com>
2022-02-17 10:27:12 +0530

    commit

---
+++ app/src/main/java/com/example/rxdebounce/Debounce.java
@@ -0,0 +1,81 @@
+package com.example.rxdebounce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.jakewharton.rxbinding2.widget.RxTextView;
+import com.jakewharton.rxbinding2.widget.TextViewTextChangeEvent;
+
+import java.util.concurrent.TimeUnit;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class Debounce extends AppCompatActivity {
+
+    private static final String TAG = Debounce.class.getSimpleName();
+
+    private CompositeDisposable disposable = new CompositeDisposable();
+    private Unbinder unbinder;
+    @BindView(R.id.input_search)
+    EditText inputSearch;
+
+    @BindView(R.id.txt_search_string)
+    TextView txtSearchString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debounce);
+        unbinder = ButterKnife.bind(this);
+
+        disposable.add(
+                RxTextView.textChangeEvents(inputSearch)
+                        .skipInitialValue()
+                        .debounce(300, TimeUnit.MILLISECONDS)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(searchQuery()));
+
+        txtSearchString.setText("Search query will be accumulated every 300 milli sec");
+    }
+
+    private DisposableObserver<TextViewTextChangeEvent> searchQuery() {
+        return new DisposableObserver<TextViewTextChangeEvent>() {
+            @Override
+            public void onNext(TextViewTextChangeEvent textViewTextChangeEvent) {
+                Log.d(TAG, "search string: " + textViewTextChangeEvent.text().toString());
+
+                txtSearchString.setText("Query: " + textViewTextChangeEvent.text().toString());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+        disposable.clear();
+    }
+}
+
+
